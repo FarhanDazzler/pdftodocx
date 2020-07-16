3867d32617cca7759be4ce9c36e556912ad8472f
Hassan Bari Raza <hbr8218@gmail.com>
2020-07-16 08:32:21 +0530

    working fine, not get stored in downloads folder

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import os 
+
+from package.pdftoword import *
+
+app = Flask(__name__)
+# add 'uploads' folder to the project's root directory where uploaded files could be saved
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # Limit the size of pdf file
+
+
+ALLOWED_EXTENSIONS = {'pdf'}
+def allowed_file(filename):
+   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   if request.method == 'POST':
+       if 'file' not in request.files:
+           print('No file attached in request')
+           return redirect(request.url)
+       file = request.files['file']
+       if file.filename == '':
+           print('No file selected')
+           return redirect(request.url)
+       if file and allowed_file(file.filename):
+           filename = secure_filename(file.filename)
+           print("filename={}".format(os.path.abspath(filename)))
+           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           print("Uploaded successfully")
+           # my conversion code
+           print(filename)
+           res = pdfToDocx(filename=os.path.join(app.config['UPLOAD_FOLDER'], filename),
+                     remote_name=filename,
+                      output_name=filename)
+           print("response from server", res)
+        #    process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+        #    return redirect(url_for('uploaded_file', filename=filename))
+   return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+   return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ package/pdftoword.py
@@ -0,0 +1,87 @@
+import groupdocs_conversion_cloud
+from flask import send_from_directory
+from shutil import copyfile
+import os
+
+from app import DOWNLOAD_FOLDER, app
+# add 'uploads' folder to the project's root directory where uploaded files could be saved
+
+
+# groupdocx API settings
+api_sid = os.environ.get('APP_SID')
+api_key = os.environ.get('APP_KEY')
+
+def pdfToDocx(filename, remote_name, output_name):
+        app_sid = api_sid
+        app_key = api_key
+
+        # Create instance of the API
+        convert_api = groupdocs_conversion_cloud.ConvertApi.from_keys(app_sid, app_key)
+        file_api = groupdocs_conversion_cloud.FileApi.from_keys(app_sid, app_key)
+
+        try:
+
+                #upload soruce file to storage
+                filename = filename
+                remote_name = remote_name
+                output_name= remote_name.rsplit('.')[0]+'.docx'
+                strformat='docx'
+                print(filename, remote_name, output_name)
+
+                request_upload = groupdocs_conversion_cloud.UploadFileRequest(remote_name,filename)
+                print("upload request")
+                response_upload = file_api.upload_file(request_upload)
+                print("uploaded to the cloud")
+                        #Convert PDF to Word document
+                settings = groupdocs_conversion_cloud.ConvertSettings()
+                settings.file_path =remote_name
+                settings.format = strformat
+                settings.output_path = output_name
+                print("converted to docx")
+
+                loadOptions = groupdocs_conversion_cloud.PdfLoadOptions()
+                loadOptions.hide_pdf_annotations = True
+                loadOptions.remove_embedded_files = False
+                loadOptions.flatten_all_fields = True
+
+                settings.load_options = loadOptions
+
+                convertOptions = groupdocs_conversion_cloud.DocxConvertOptions()
+                convertOptions.from_page = 1
+                convertOptions.pages_count = 1
+
+                settings.convert_options = convertOptions
+
+                request = groupdocs_conversion_cloud.ConvertDocumentRequest(settings)
+                response = convert_api.convert_document(request)
+                
+                print("Document converted successfully: " + str(response))
+                # Download request
+                request_download = groupdocs_conversion_cloud.DownloadFileRequest(output_name)
+                response_download = file_api.download_file(request_download)
+                print("Response Download ", response_download)
+                copyfile(response_download,'3rd.docx')
+                send_from_directory(os.path.abspath(''),
+                               '3rd.docx', as_attachment=True)
+                print("Successful")
+        except groupdocs_conversion_cloud.ApiException as e:
+                print("Exception when calling get_supported_conversion_types: {0}".format(e.message))
+        
+
+def downloadDocx(filename, saved_filenme):
+
+        # Create instance of the API
+        convert_api = groupdocs_conversion_cloud.ConvertApi.from_keys(app_sid, app_key)
+        file_api = groupdocs_conversion_cloud.FileApi.from_keys(app_sid, app_key)
+
+        try:
+
+                #upload soruce file to storage
+                filename = filename
+
+                request_download = groupdocs_conversion_cloud.DownloadFileRequest(filename)
+                response_download = file_api.download_file(request_download)
+                copyfile(response_download, saved_filenme)
+
+        except groupdocs_conversion_cloud.ApiException as e:
+                print("Exception : {0}".format(e.message))
